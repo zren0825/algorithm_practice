cb63d3ea7b41f313d040d3384af1bbcb52a812a4
Ryan Ren <zhengshuangren@gmail.com>
2019-04-22 14:04:42 -0700

    update TSP

--- src/com/company/A/Zulily/TravellingSalesman.java
+++ src/com/company/A/Zulily/TravellingSalesman.java
@@ -16,28 +16,30 @@ public class TravellingSalesman {
     // 这里input理论上不应该是这样 应该给一个完整的图 不过这样的input逻辑上是够的
     public List<City> shortestWayToVisitAllCity(City startCity, int numCities) {
         List<City> shortestWay = new LinkedList<>();
-        shortestWayToVisitAllCity(shortestWay, new LinkedList<>(), new HashSet<>(), new int[]{0}, 0, startCity, numCities);
+        Set<String> visited = new HashSet<>();
+        visited.add(startCity.name);
+        shortestWayToVisitAllCity(shortestWay, new LinkedList<>(), startCity, startCity, 0, new int[]{Integer.MAX_VALUE}, numCities, visited);
         return shortestWay;
     }
 
-    private void shortestWayToVisitAllCity(List<City> shortestWay, List<City> curOrder, Set<String> visited, int[] minDist, int dist, City cur, int numCities) {
-        if (curOrder.size() == numCities && dist < minDist[0]) {
-            shortestWay = new LinkedList<>(curOrder);
-            minDist[0] = dist;
+    private void shortestWayToVisitAllCity(List<City> shortestWay, List<City> curOrder, City startCity, City cur, int dist, int[] minDist, int numCities, Set<String> visited ) {
+        if(curOrder.size() == numCities) {
+            Integer returnDist = cur.neis.get(startCity.name);
+            if(returnDist != null && returnDist + dist < minDist[0]) {
+                shortestWay = new LinkedList<>(curOrder);
+                minDist[0] = dist + returnDist;
+            }
             return;
         }
-        if (curOrder.size() == numCities) {
-            return;
-        }
-        for (Map.Entry<City, Integer> entry : cur.neis.entrySet()) {
+        for(Map.Entry<City, Integer> entry: cur.neis.entrySet()) {
             City nextCity = entry.getKey();
-            if (visited.contains(nextCity.name)) {
+            if(visited.contains(nextCity.name)) {
                 continue;
             }
             Integer distToNextCity = entry.getValue();
             curOrder.add(nextCity);
             visited.add(nextCity.name);
-            shortestWayToVisitAllCity(shortestWay, curOrder, visited, minDist, dist + distToNextCity, nextCity, numCities);
+            shortestWayToVisitAllCity(shortestWay, curOrder, startCity, nextCity, dist + distToNextCity, minDist, numCities, visited);
             curOrder.remove(curOrder.size() - 1);
             visited.remove(nextCity.name);
         }
