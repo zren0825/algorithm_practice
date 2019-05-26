76a5214e464236b035c6293c565cf5565e5eec3b
Ryan Ren <zhengshuangren@gmail.com>
2019-05-26 01:25:56 -0700

    auto format

--- src/com/company/LC/L322CoinChange.java
+++ src/com/company/LC/L322CoinChange.java
@@ -4,10 +4,10 @@ public class L322CoinChange {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
         dp[0] = 0;
-        for(int i = 1; i <= amount; i++){
+        for (int i = 1; i <= amount; i++) {
             dp[i] = amount + 1;
-            for(int coin: coins){
-                if(coin <= i){
+            for (int coin : coins) {
+                if (coin <= i) {
                     dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
             }
--- src/com/company/LC/L518CoinChangeII.java
+++ src/com/company/LC/L518CoinChangeII.java
@@ -4,10 +4,10 @@ public class L518CoinChangeII {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
         dp[0] = 1;
-        for(int j = 0; j < coins.length; j++){
-            for(int i = 1; i <= amount; i++){
-                if(i >= coins[j])
-                    dp[i] += dp[i- coins[j]];
+        for (int j = 0; j < coins.length; j++) {
+            for (int i = 1; i <= amount; i++) {
+                if (i >= coins[j])
+                    dp[i] += dp[i - coins[j]];
             }
         }
         return dp[amount];
